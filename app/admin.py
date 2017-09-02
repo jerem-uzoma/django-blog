fc04671061bcd82e022596cadaa30dd5d6a6090a
Uzoma <nwokouzomaje@hotmail.com>
2017-09-02 16:55:07 +0100

    Third commit, made some changes to the admin.py and the model.py files

--- app/admin.py
+++ app/admin.py
@@ -3,4 +3,12 @@ from .models import Article, Category, BlogComment, Tag
 
 # Register your models here.
 
-admin.site.register([Article, Category, BlogComment, Tag])
+class ArticleAdmin(admin.ModelAdmin):
+    list_display=("title","body","created_time",)
+	
+class BlogCommentAdmin(admin.ModelAdmin):
+    list_display =("user_name", "body","created_time")
+
+admin.site.register(BlogComment, BlogCommentAdmin)
+admin.site.register(Article,ArticleAdmin)
+admin.site.register([Category, Tag])
